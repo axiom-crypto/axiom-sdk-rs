85cd1f429a35a19ecb718688ab6123dfad2f6fed
roshan <19766713+rpalakkal@users.noreply.github.com>
2024-05-06 18:22:20 -0400

    feat: assign_groth16_input_with_known_vk

--- sdk/src/subquery/groth16.rs
+++ sdk/src/subquery/groth16.rs
@@ -1,5 +1,5 @@
 use axiom_circuit::{
-    axiom_eth::halo2curves::bn256::Fr,
+    axiom_eth::{halo2_base::AssignedValue, halo2curves::bn256::Fr},
     subquery::groth16::{Groth16AssignedInput, Groth16Input},
 };
 
@@ -26,6 +26,27 @@ pub fn assign_groth16_input(
         .collect::<Vec<_>>();
     Groth16AssignedInput {
         vkey_bytes: assigned_vkey.try_into().unwrap(),
+        proof_bytes: assigned_proof.try_into().unwrap(),
+        public_inputs: assigned_public_inputs.try_into().unwrap(),
+    }
+}
+
+pub fn assign_groth16_input_with_known_vk(
+    api: &mut AxiomAPI,
+    assigned_vk: Vec<AssignedValue<Fr>>,
+    proof_bytes: Vec<Fr>,
+    pi: Vec<Fr>,
+) -> Groth16AssignedInput<Fr> {
+    let assigned_proof = proof_bytes
+        .iter()
+        .map(|v| api.ctx().load_witness(*v))
+        .collect::<Vec<_>>();
+    let assigned_public_inputs = pi
+        .iter()
+        .map(|v| api.ctx().load_witness(*v))
+        .collect::<Vec<_>>();
+    Groth16AssignedInput {
+        vkey_bytes: assigned_vk,
         proof_bytes: assigned_proof.try_into().unwrap(),
         public_inputs: assigned_public_inputs.try_into().unwrap(),
     }
